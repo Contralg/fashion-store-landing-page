ecad703d048b8bdc264af619aa39f27b8a2eb054
Contralg <alfiaziz003@outlook.com>
2025-07-17 16:10:40 +0700

    Edit config Next JS

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone', // optional tapi disarankan
+  basePath: '/portofolio1',
+  trailingSlash: true, // opsional, tergantung preferensi  
 };
 
 export default nextConfig;
